b509fd4fd445e969d076a32955bbcdcee3738afa
Chad Stark <chad.d.stark@hotmail.com>
2021-07-16 16:35:49 -0700

    prettified the files

--- src/App.js
+++ src/App.js
@@ -1,41 +1,41 @@
-import "./App.css";
-import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
-import { Layout } from "antd";
+import './App.css';
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
+import { Layout } from 'antd';
 
-import Footer from "./components/Footer";
-import { Navbar } from "./Navbar";
+import Footer from './components/Footer';
+import { Navbar } from './Navbar';
 
-import Home from "./pages/Home";
-import Weather from "./pages/Weather";
-import Movies from "./pages/Movies";
-import Books from "./pages/Books";
-import Jokes from "./pages/Jokes";
+import Home from './pages/Home';
+import Weather from './pages/Weather';
+import Movies from './pages/Movies';
+import Books from './pages/Books';
+import Jokes from './pages/Jokes';
 
 const { Content } = Layout;
 
 function App() {
-  return (
-    <Router>
-      <Navbar />
+    return (
+        <Router>
+            <Navbar />
 
-      <Content
-        style={{
-          padding: "0 50px",
-        }}
-      >
-        <div className="site-layout-content">
-          <Switch>
-            <Route path="/" exact component={Home}></Route>
-            <Route path="/weather" component={Weather}></Route>
-            <Route path="/movies" component={Movies}></Route>
-            <Route path="/books" component={Books}></Route>
-            <Route path="/jokes" component={Jokes}></Route>
-          </Switch>
-        </div>
-      </Content>
-      <Footer />
-    </Router>
-  );
+            <Content
+                style={{
+                    padding: '0 50px',
+                }}
+            >
+                <div className="site-layout-content">
+                    <Switch>
+                        <Route path="/" exact component={Home}></Route>
+                        <Route path="/weather" component={Weather}></Route>
+                        <Route path="/movies" component={Movies}></Route>
+                        <Route path="/books" component={Books}></Route>
+                        <Route path="/jokes" component={Jokes}></Route>
+                    </Switch>
+                </div>
+            </Content>
+            <Footer />
+        </Router>
+    );
 }
 
 export default App;
--- src/pages/Jokes.js
+++ src/pages/Jokes.js
@@ -1,11 +1,11 @@
 import React from 'react';
 
 function Jokes() {
-  return (
-    <div>
-      <h1>Jokes</h1>
-    </div>
-  );
+    return (
+        <div>
+            <h1>Jokes</h1>
+        </div>
+    );
 }
 
 export default Jokes;
