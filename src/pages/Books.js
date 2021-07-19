7b399a93f0321139dfe068649547f6e90d1ca2e6
Chad Stark <chad.d.stark@hotmail.com>
2021-07-19 16:42:22 -0700

    began setting up the books page

--- src/pages/Books.js
+++ src/pages/Books.js
@@ -1,9 +1,27 @@
-import React from 'react';
+import React, { useState } from 'react';
+import dotenv from 'dotenv';
+import axios from 'axios';
+import { Input } from 'antd';
+dotenv.config();
 
+const { Search } = Input;
 function Books() {
+    const [isBook, setIsBook] = useState('');
+
+    const handleClick = () => {};
     return (
         <div>
             <h1>Books</h1>
+            <div className="search-wrapper">
+                <Search
+                    placeholder="input search text"
+                    onSearch={handleClick}
+                    onChange={(e) => {
+                        setIsBook(e.target.value);
+                    }}
+                    enterButton
+                />
+            </div>
         </div>
     );
 }
