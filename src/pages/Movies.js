e40ca0aefacb28f8287178a7333168908e02d8de
Kyle Garcia <kyle.fire@gmail.com>
2021-07-19 14:42:20 +0900

    input bar added with functionality

--- src/pages/Movies.js
+++ src/pages/Movies.js
@@ -1,28 +1,62 @@
 import React, { useState, useEffect } from 'react';
-// import { Input, Space, Card } from 'antd';
+import { Input, Space, Card } from 'antd';
 import axios from 'axios';
+
+const { Search } = Input;
 
 function Movies() {
     const [movieList, setMovieList] = useState([]);
     const [loading, setLoading] = useState(true);
+    const [movieSearch, setMovieSearch] = useState('');
 
-    useEffect(() => {
+    function getMovies() {
+        console.log('asah');
         axios
-            .get('http://www.omdbapi.com/?apikey=a37a0fb5&s=star_wars')
+            .get(`http://www.omdbapi.com/?apikey=a37a0fb5&s=${movieSearch}`)
             .then((res) => {
                 setMovieList(res.data);
                 setLoading(false);
             })
             .catch(console.error);
-    }, []);
+    }
     console.log(movieList);
 
     if (loading === true) {
-        return <h1>Loading...</h1>;
+        return (
+            <div>
+                <h1>Movies</h1>
+                <Space direction="vertical">
+                    <Search
+                        placeholder="Enter a movie name"
+                        allowClear
+                        onChange={(e) => {
+                            setMovieSearch(e.target.value);
+                            console.log(movieSearch);
+                        }}
+                        onSearch={getMovies}
+                        style={{ width: 200 }}
+                    />
+                </Space>
+            </div>
+        );
     }
     return (
         <div>
             <h1>Movies</h1>
+            <div>
+                <Space direction="vertical">
+                    <Search
+                        placeholder="Enter a movie name"
+                        allowClear
+                        onChange={(e) => {
+                            setMovieSearch(e.target.value);
+                            console.log(movieSearch);
+                        }}
+                        onSearch={getMovies}
+                        style={{ width: 200 }}
+                    />
+                </Space>
+            </div>
             <img
                 src={movieList.Search[0].Poster}
                 alt={movieList.Search[0].Title}
