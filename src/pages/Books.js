81308eb59913455c9e0cb399c6a1821e99d49c65
Chad Stark <chad.d.stark@hotmail.com>
2021-07-20 11:14:57 -0700

    added some css and finished rendering books

--- src/pages/Books.js
+++ src/pages/Books.js
@@ -1,23 +1,81 @@
 import React, { useState } from 'react';
 import dotenv from 'dotenv';
 import axios from 'axios';
-import { Input } from 'antd';
+import { Input, Card, Avatar, Row } from 'antd';
+import '../styling/Books.css';
 dotenv.config();
 
 const { Search } = Input;
 function Books() {
     const [isBook, setIsBook] = useState('');
+    const [isData, setIsData] = useState([]);
+    const [isRenderData, setIsRenderData] = useState(false);
 
     const google_books_api_key = process.env.REACT_APP_GOOGLE_BOOKS_API_KEY;
+
+    const { Meta } = Card;
 
     const handleClick = () => {
         axios
             .get(
-                `https://www.googleapis.com/books/v1/volumes?q=${isBook}+intitle:keyes&key=${google_books_api_key}`
+                `https://www.googleapis.com/books/v1/volumes?q=+intitle:${isBook}&key=${google_books_api_key}`
             )
             .then((res) => {
-                console.log(res);
+                // console.log(res.data.items);
+                setIsData(res.data.items);
+                setIsRenderData(true);
+                // for (var i = 0; i < res.data.items.length; i++) {
+                //     console.log(res.data.items[i].volumeInfo.title);
+                // }
             });
+    };
+
+    // const renderCards = () => {
+    //     return (
+    //         <Card
+    //             style={{ width: 300 }}
+    //             cover={
+    //                 <img
+    //                     alt="example"
+    //                     src={element.volumeInfo.imageLinks.thumbnail}
+    //                 />
+    //             }
+    //         >
+    //             <Meta
+    //                 avatar={
+    //                     <Avatar src="https://zos.alipayobjects.com/rmsportal/ODTLcjxAfvqbxHnVXCYX.png" />
+    //                 }
+    //                 title="Card title"
+    //                 description="This is the description"
+    //             />
+    //         </Card>
+    //     );
+    // };
+
+    const renderDataCards = (data) => {
+        console.log(data);
+        return (
+            <Card
+                className="card-data"
+                style={{ width: 300 }}
+                cover={
+                    <img
+                        alt="example"
+                        src={data.volumeInfo.imageLinks.thumbnail}
+                    />
+                }
+            >
+                <Meta
+                    avatar={
+                        <Avatar
+                            src={data.volumeInfo.imageLinks.smallThumbnail}
+                        />
+                    }
+                    title={data.volumeInfo.title}
+                    description={data.volumeInfo.description}
+                />
+            </Card>
+        );
     };
 
     return (
@@ -32,6 +90,9 @@ function Books() {
                     }}
                     enterButton
                 />
+            </div>
+            <div className="bookdata-wrapper">
+                <Row>{isData.map(renderDataCards)}</Row>
             </div>
         </div>
     );
